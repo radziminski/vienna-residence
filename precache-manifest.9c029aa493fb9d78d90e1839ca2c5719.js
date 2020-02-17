b3181e5eeb9a009da0fc9291aa565f4a72b2575d
Jan Radziminski <radziminski.j@gmail.com>
2020-02-17 10:16:26 +0100

    Updates

---
+++ precache-manifest.9c029aa493fb9d78d90e1839ca2c5719.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52dd4884c5955849541b32dd94a6866d",
+    "url": "/vienna-residence/index.html"
+  },
+  {
+    "revision": "48029e7ee83733a9fc80",
+    "url": "/vienna-residence/static/css/main.a83c3b94.chunk.css"
+  },
+  {
+    "revision": "f1db27d6fd71707cb39a",
+    "url": "/vienna-residence/static/js/2.1d46eba8.chunk.js"
+  },
+  {
+    "revision": "05182bf3097604151dd2f67e79167c1a",
+    "url": "/vienna-residence/static/js/2.1d46eba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48029e7ee83733a9fc80",
+    "url": "/vienna-residence/static/js/main.63a296de.chunk.js"
+  },
+  {
+    "revision": "b49689faed4a7316039b",
+    "url": "/vienna-residence/static/js/runtime-main.46bfae75.js"
+  },
+  {
+    "revision": "76a593f89ad7545c5c07277da239e442",
+    "url": "/vienna-residence/static/media/business.76a593f8.jpg"
+  },
+  {
+    "revision": "358d3ed1e3f6b8f0eab94b2ee579e36d",
+    "url": "/vienna-residence/static/media/header-bg.358d3ed1.jpg"
+  },
+  {
+    "revision": "22ca6da4a5aa4cb9e366c5c0439ac68f",
+    "url": "/vienna-residence/static/media/restaurant.22ca6da4.jpg"
+  },
+  {
+    "revision": "d0cb6c24ed76c94d7919f32a10f239ce",
+    "url": "/vienna-residence/static/media/room-queen-exc.d0cb6c24.jpg"
+  },
+  {
+    "revision": "4fced7550c6fc1bb7d599fdacdd25fbb",
+    "url": "/vienna-residence/static/media/room-queen.4fced755.jpg"
+  },
+  {
+    "revision": "df6de2ee4e8f871e1f260adce823a540",
+    "url": "/vienna-residence/static/media/room-single.df6de2ee.jpg"
+  },
+  {
+    "revision": "652c8726d758bf4db5647dbd0d849e2d",
+    "url": "/vienna-residence/static/media/room-two-bed.652c8726.jpg"
+  },
+  {
+    "revision": "8cf2260377ee81ca735aa1520b8bde98",
+    "url": "/vienna-residence/static/media/spa.8cf22603.jpg"
+  },
+  {
+    "revision": "ec308125d5d002bfcbf356ac8401dd50",
+    "url": "/vienna-residence/static/media/sport.ec308125.jpg"
+  },
+  {
+    "revision": "1fb179e4921c4cc0eec34537a80a1b21",
+    "url": "/vienna-residence/static/media/transport-bg.1fb179e4.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vienna-residence/precache-manifest.8ce79c4f3aa7c80e94cbdad22f8b3250.js"
+  "/vienna-residence/precache-manifest.9c029aa493fb9d78d90e1839ca2c5719.js"
 );
 
 self.addEventListener('message', (event) => {
