93e3170dda4912bae069e34d51a2fd84109b024d
Jan Radziminski <radziminski.j@gmail.com>
2020-02-25 23:57:21 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vienna-residence/precache-manifest.2b5c1dd6fb9fe57c7188359800e992fc.js"
+  "/vienna-residence/precache-manifest.ff9d6dc1821d1cde7b34abae276bfa85.js"
 );
 
 self.addEventListener('message', (event) => {
